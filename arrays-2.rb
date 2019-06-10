894318bd265684d4b28c2241ebe2b18c70bc9681
Carlo Munguia <carlomunguia@carlos-air.home>
2019-06-10 16:03:12 -0500

    nested eaches!

--- arrays-2.rb
+++ arrays-2.rb
@@ -50,8 +50,23 @@ fives.each do |number|
   end
 end
 
-fives.each { |number| number.even? ? evens << number : odds << number }
+fives.each {|number| number.even? ? evens << number : odds << number}
 
 p evens
 p odds
+
+#EACH WITHIN EACH
+
+shirts = ["striped", "plain white", "band", "plaid"]
+ties = ["polka dot", "solid color", "boring"]
+
+shirts.each do |shirt|
+  ties.each do |tie|
+   puts "OPTION: A #{shirt} and a #{tie} tie"
+  end
+end
+
+
+
+
 
