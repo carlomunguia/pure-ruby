5c2eca5d0f341a229544c23237bdbc2f8f83cddd
Carlo Munguia <carlomunguia@carlos-air.home>
2019-07-27 14:47:19 -0500

    end of regexp

--- reggie-x.rb
+++ reggie-x.rb
@@ -58,7 +58,35 @@ puts phrase.scan(/\a. \a. \a/)
 
 paragraph = "This is my reality, dipped in deep blue pearl.  Feel the blue house where she sighs alone at nights."
 
-p paragraph.scan(/\D/).length
+p paragraph.scan(/\D/)
+
+# REGEX ANCHORS
+
+poem = "99 bottles of kombucha on the wall. 99 bottles of kombucha"
+
+p poem.scan(/cha\z/)
+
+sales = "I bought 9 apples, 29 bananas, and 4 oranges at the store."
+
+p sales.scan(/[^aeiouAEIOU,\s\d\.]/).size
+
+# SUB / GSUB METHODS
+
+puts "ufo".sub("u", "z")
+
+word = 'aspirin'
+
+p word.sub("as", "bass")
+
+puts "a mango".gsub("m", "Zha-")
+
+puts "(444)555-6666".gsub(/[-\s\(\)]/, "")
+
+numbers.gsub(/[-\s(\)]/, "")
+
+
+
+
 
 
 
