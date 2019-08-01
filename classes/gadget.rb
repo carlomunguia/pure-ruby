ea6e847c4992d66201c617ebc2f73d1f54c0999e
Carlo Munguia <carlomunguia@carlos-air.home>
2019-08-01 16:44:26 -0500

    work

--- classes/gadget.rb
+++ classes/gadget.rb
@@ -1,39 +1,29 @@
 class Gadget
-
   def initialize
     @username = "User #{rand(1..100)}"
     @password = "topsecret"
-    @production_number = "#{("a".."z").to_a.sample}"
+    @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+  end
+
+  def info
+    "Gadget #{@production_number} has the username #{@username}"
   end
 end
 
 phone = Gadget.new
-p phone.inspect
 laptop = Gadget.new
-p laptop.inspect
-microwave = Gadget.new
-p microwave.instance_variables
 
-puts phone
-puts laptop
-puts microwave
+puts phone.info
+puts laptop.info
+
+puts phone.methods - Object.methods
 
 
-puts phone.respond_to?(:class)
 
-shiny = Gadget.new
-flashy = Gadget.new
 
-p shiny.object_id
-p flashy.object_id
 
-glossy = shiny
 
-p glossy.object_id
 
-p glossy == shiny
-p shiny == flashy
-p glossy == flashy
 
 
 
