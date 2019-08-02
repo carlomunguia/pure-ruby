06e983e6e4c30d6a5865555882eda3947957d89a
Carlo Munguia <carlomunguia@carlos-air.home>
2019-08-01 19:27:10 -0500

    working with instances/methods

--- classes/gadget.rb
+++ classes/gadget.rb
@@ -5,18 +5,27 @@ class Gadget
     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
   end
 
-  def info
-    "Gadget #{@production_number} has the username #{@username}"
+  def to_s
+    "Gadget #{@production_number} has the username #{@username}.
+     It is made from the #{self.class} and it has the ID #{self.object_id}"
+  end
+
+  def username
+    @username
+  end
+
+  def production_number
+    @production_number
   end
 end
 
 phone = Gadget.new
+p phone.username
+p phone.production_number
 laptop = Gadget.new
 
-puts phone.info
-puts laptop.info
 
-puts phone.methods - Object.methods
+
 
 
 
