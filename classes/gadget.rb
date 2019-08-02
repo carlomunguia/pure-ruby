c067d570ee38e921419a5ccc49d478d2c2f3b055
Carlo Munguia <carlomunguia@carlos-air.home>
2019-08-01 21:55:36 -0500

    end of classes 1

--- classes/gadget.rb
+++ classes/gadget.rb
@@ -1,7 +1,12 @@
 class Gadget
-  def initialize
-    @username = "User #{rand(1..100)}"
-    @password = "topsecret"
+
+  attr_accessor :username
+  attr_reader :production_number
+  attr_writer :password
+
+  def initialize(username, password)
+    @username = username
+    @password = password
     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
   end
 
@@ -9,21 +14,12 @@ class Gadget
     "Gadget #{@production_number} has the username #{@username}.
      It is made from the #{self.class} and it has the ID #{self.object_id}"
   end
-
-  def username
-    @username
-  end
-
-  def production_number
-    @production_number
-  end
 end
 
-phone = Gadget.new
-p phone.username
-p phone.production_number
-laptop = Gadget.new
+puts g1 = Gadget.new("rubyfan102", "programming123")
 
+puts g2 = Gadget.new("misterprogrammer", "bestpassever")
+g2.username = "Mrs.Programmer"
 
 
 
