5bb893b0023c79b0df33e45d9011b253a0d6230f
Carlo Munguia <carlomunguia@carlos-air.home>
2019-08-02 14:34:18 -0500

    privacy!

--- classes/gadget.rb
+++ classes/gadget.rb
@@ -1,25 +1,38 @@
 class Gadget
 
-  attr_accessor :username
-  attr_reader :production_number
   attr_writer :password
+  attr_reader :production_number
+  attr_accessor :username
 
   def initialize(username, password)
     @username = username
     @password = password
-    @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+    @production_number = generate_production_number
   end
 
   def to_s
     "Gadget #{@production_number} has the username #{@username}.
      It is made from the #{self.class} and it has the ID #{self.object_id}"
   end
+
+  private
+
+  def generate_production_number
+    start_digits = rand(10000..99999)
+    end_digits = rand(10000..99999)
+    alphabet = ("A".."Z").to_a
+    middle_digits = "2019"
+    5.times { middle_digits << alphabet.sample }
+    "#{start_digits}-#{middle_digits}-#{end_digits}"
+  end
 end
 
-puts g1 = Gadget.new("rubyfan102", "programming123")
+phone = Gadget.new("user", "password")
+p phone.generate_production_number
 
-puts g2 = Gadget.new("misterprogrammer", "bestpassever")
-g2.username = "Mrs.Programmer"
+
+
+
 
 
 
--- modules/enumerable_city.rb
+++ modules/enumerable_city.rb
@@ -1,1 +1,31 @@
+class ConvenienceStore
+  include Enumerable
+  attr_reader :snacks
+
+  def initialize
+    @snacks = []
+  end
+
+  def add_snack(snack)
+    snacks << snack
+  end
+
+  def each
+    snacks.each do |snack|
+      yield snack
+    end
+  end
+end
+
+bodega = ConvenienceStore.new
+bodega.add_snack("Lime Soda")
+bodega.add_snack("Crybabies")
+bodega.add_snack("Jolly Ranchers")
+
+p bodega.snacks
+p bodega.map { |snack| snack.upcase }
+p bodega.select { |snack| snack.downcase.include?("j") }
+
+p bodega.sort
+
 
