466fddf475786014ff3dc6a2f74e4dd88167a63a
Carlo Munguia <carlomunguia@carlos-air.home>
2019-08-02 19:43:12 -0500

    finally fixed end placement typo aye yae yae

--- classes/gadget.rb
+++ classes/gadget.rb
@@ -1,12 +1,14 @@
-class Gadget
+require_relative 'app_store'
 
-  attr_reader :production_number
+class Gadget
+  attr_reader :production_number, :apps
   attr_accessor :username
 
   def initialize(username, password)
     @username = username
     @password = password
     @production_number = generate_production_number
+    @apps = []
   end
 
   def to_s
@@ -14,25 +16,44 @@ class Gadget
      It is made from the #{self.class} and it has the ID #{object_id}"
   end
 
+  def reset(username, password)
+    self.username = username
+    self.password = password
+    self.apps = []
+  end
+
   def password=(new_password)
     @password = new_password if validate_password(new_password)
   end
+
+  def install_app(name)
+    app = AppStore.find_app(name)
+    @apps << app unless @apps.include?(app)
+  end
+
+  def delete_app(name)
+    app = apps.find { |installed_app| installed_app.name == name }
+    apps.delete(app) unless app.nil?
+  end
+
+
+  private
+
+  attr_writer :apps
+
+  def generate_production_number
+    start_digits = rand(10000..99999)
+    end_digits = rand(10000..99999)
+    alphabet = ("A".."Z").to_a
+    middle_digits = "2019"
+    5.times { middle_digits << alphabet.sample }
+    "#{start_digits}-#{middle_digits}-#{end_digits}"
+  end
+
+  def validate_password(new_password)
+    new_password.is_a?(String) && new_password.length >= 6 && new_password =~ /\d/
+  end
 end
 
-private
-
-def generate_production_number
-  start_digits = rand(10000..99999)
-  end_digits = rand(10000..99999)
-  alphabet = ("A".."Z").to_a
-  middle_digits = "2019"
-  5.times { middle_digits << alphabet.sample }
-  "#{start_digits}-#{middle_digits}-#{end_digits}"
-end
-
-def validate_password(new_password)
-  new_password.is_a?(String) && new_password.length >= 6 && new_password =~ /\d/
-end
-
-phone = Gadget.new("rubygod102", "programming123")
-
+g = Gadget.new("carlo", "password")
+p g.apps
