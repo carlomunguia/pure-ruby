c0dc96a629fd52fa9b13682b670d54a94fb026d3
Carlo Munguia <carlomunguia@carlos-air.home>
2019-08-04 16:16:53 -0500

    super super duper super duper

---
+++ classes/assembly.rb
@@ -0,0 +1,50 @@
+class Product
+  @@product_counter = 0
+
+  def self.counter
+    @@product_counter
+  end
+
+  def initialize
+    @@product_counter += 1
+  end
+end
+
+class Widget < Product
+  @@widget_counter = 0
+
+  def self.counter
+    @@widget_counter
+  end
+
+  def initialize
+    super
+    @@widget_counter += 1
+  end
+end
+
+class Thingamajig < Product
+  @@thingamajig_counter = 0
+
+  def self.counter
+    @@thingamajig_counter
+  end
+
+  def initialize
+    super
+    @@thingamajig_counter += 1
+  end
+end
+
+a = Widget.new
+b = Widget.new
+
+puts Product.counter
+
+c = Thingamajig.new
+d = Thingamajig.new
+e = Thingamajig.new
+
+puts Thingamajig.counter
+puts Product.counter
+
