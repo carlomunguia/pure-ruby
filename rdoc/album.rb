a933dcd59b491c19186df737f104f75d7c0912db
Carlo Munguia <carlomunguia@carlos-air.home>
2019-08-05 20:54:32 -0500

    end of the line, sonny

--- rdoc/album.rb
+++ rdoc/album.rb
@@ -1,15 +1,21 @@
+#An Album class that stores an Array of Songs
 class Album
   include Enumerable
+
+  #An Array Object of Songs. Each song is a String
   attr_reader :songs
 
+  #Create a new album instance with an Empty Song Array!
   def initialize
     @songs = []
   end
 
+  #Add a song to the Album object's song array
   def add_song(song)
     songs << song
   end
 
+  #Yield each song in the Album to a block
   def each
     songs.each do |song|
       yield song
